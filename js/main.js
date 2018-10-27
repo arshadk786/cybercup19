1ae1c3cb5e187dd1dd5dbfa0d93ef6295c85475e
Arshad Khan <40868106+arshadk786@users.noreply.github.com>
2018-10-27 22:39:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+function add_chatinline(){var hccid=45898428;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
+add_chatinline();
 (function($) {
 	"use strict"
 
